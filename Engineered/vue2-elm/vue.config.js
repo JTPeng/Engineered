2effe74849192755f994ca5a123e2f0c8540915b
JTPeng <404502753@qq.com>
2022-08-03 15:10:56 +0800

    dll分包

--- Engineered/vue2-elm/vue.config.js
+++ Engineered/vue2-elm/vue.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require("webpack");
+const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
+
 console.log("process.env.MEASURE", process.env.MEASURE);
 const smp = new SpeedMeasurePlugin({
   disable: !(process.env.MEASURE === "true"), // 是否开启 true不启动
   outputFormat: "human",
 });
 module.exports = {
+  publicPath: "./",
   parallel: true,
   configureWebpack: smp.wrap({
     resolve: {
@@ -35,6 +39,14 @@ module.exports = {
     plugins: [
       new BundleAnalyzer({
         analyzerMode: process.env.BUILD === "true" ? "server" : "false",
+      }),
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: path.resolve(__dirname, "./dll/vue-manifest.json"), // 排除掉的内容
+      }),
+      new AddAssetHtmlPlugin({
+        // 需要引入的是dll内的dll文件,而不是dist目录下,因为htmlWebpackPlugin在工作时,dll.js文件还没生成
+        filepath: path.resolve(__dirname, "./dll/vue.dll.js"),
       }),
     ],
   }),
