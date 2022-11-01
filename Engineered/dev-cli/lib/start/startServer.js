93b9c19a1ff51a597b7d5d11cb502771d1a8f73c
JTPeng <404502753@qq.com>
2022-11-01 22:30:04 +0800

    dev-cli 进程无法关闭问题

--- Engineered/dev-cli/lib/start/startServer.js
+++ Engineered/dev-cli/lib/start/startServer.js
@@ -2,10 +2,16 @@ const chokidar = require("chokidar");
 const path = require("path");
 const cp = require("child_process");
 // const devServer = require("./devServer");
+let child;
 // 1.启动服务
 function runServer() {
   const scriptPath = path.resolve(__dirname, "./devServer.js");
-  const child = cp.fork(scriptPath, ["--port 8080"]);
+  child = cp.fork(scriptPath, ["--port 8080"]);
+  child.on("exit", (code) => {
+    if (code) {
+      process.exit(1); // 退出
+    }
+  });
 }
 // 2.监听配置修改
 function runWatcher() {
